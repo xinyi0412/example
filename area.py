6d9d01c93d0435afb3e534d10c14d5004e7e8526
xinyi0412 <48367638+xinyi0412@users.noreply.github.com>
2019-03-08 16:26:44 -0500

    Add files via upload

---
+++ area.py
@@ -0,0 +1,9 @@
+#area exercise
+length = input("Please enter the length: ")
+length = float(length)
+width = input("Please enter the length: ")
+width = float(width)
+
+area = length * width
+print(area)
+
